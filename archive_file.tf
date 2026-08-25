29b2c9e60d5e34cfc5d50313cd0e56595d5aea81
JefferyChenjy <jefferychenjy@users.noreply.github.com>
2026-08-25 21:48:13 +0800

    docs: add comments to 1 terraform file

--- archive_file.tf
+++ archive_file.tf
@@ -1,3 +1,4 @@
+# This file is used to create a zip file of the lambda function code for deployment.
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "${path.module}/./lambda/index.mjs"
